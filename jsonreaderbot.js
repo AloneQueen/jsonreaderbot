111984bf5d50096b301b977438cbd25429d5f00b
Lasindu Senash <107908796+ImLasiya@users.noreply.github.com>
2022-08-05 10:21:13 +0530

    Update jsonreaderbot.js

--- jsonreaderbot.js
+++ jsonreaderbot.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const TeleBot = require('telebot');
-const bot = new TeleBot(process.env.BOT_TOKEN);
+const bot = new TeleBot(process.env.5320865354:AAH_jI-Gonos_IVHFBoLRrAQBtBhoI-73Mc);
 
 var request = require("request");
 var options = { method: 'GET',
@@ -25,7 +25,7 @@ request(options, function (error, response, body) {
   var summary = body.articles[0].description
   console.log(body);
   
-  bot.sendMessage(-242559392, '<b>${title}</b>+${summary}+${url}' , {parseMode: 'html', disable_web_page_preview: false , webPreview: urlToImage});
+  bot.sendMessage(-1001683252568, '<b>${title}</b>+${summary}+${url}' , {parseMode: 'html', disable_web_page_preview: false , webPreview: urlToImage});
   
 
 });
